48236e639ed6138a1a31e3e68017e1ac4c991d35
Alexandra Paziy <alexandrapaziy@gmail.com>
2023-11-01 01:08:38 +0200

    Added option print info about the user that should be passed to the Main function as arguments (Main.java). Added sorting algorithm implementation (BubbleSort.java, SelectionSort.java, InsertionSort.java, QuickSort.java).

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         if (args.length >= 2) {
             String name = args[0];
             String surname = args[1];
-            System.out.println("Hello! Nice to see you " + name + " " + surname + "!\n");
+            System.out.println("Hi! Nice to see you " + name + " " + surname + "!\n");
         } else {
             System.out.println("Please enter first and last name as command line arguments.");
         }
