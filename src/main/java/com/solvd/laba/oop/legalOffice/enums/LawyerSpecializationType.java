d057928ad12afdbcca4f2c1597acc6a8c8409103
Alexandra Paziy <alexandrapaziy@gmail.com>
2023-11-28 21:53:52 +0200

    Hot fix

--- src/main/java/com/solvd/laba/oop/legalOffice/enums/LawyerSpecializationType.java
+++ src/main/java/com/solvd/laba/oop/legalOffice/enums/LawyerSpecializationType.java
@@ -1,7 +1,23 @@
 package com.solvd.laba.oop.legalOffice.enums;
 
 public enum LawyerSpecializationType {
-    CIVIL_LAW,
-    CRIMINAL_LAW,
-    FAMILY_LAW,
+    CIVIL_LAW("Deals with civil matters", 2),
+    CRIMINAL_LAW("Handles criminal cases", 3),
+    FAMILY_LAW("Specializes in family-related legal issues", 1);
+
+    private final String description;
+    private final int experienceLevel;
+
+    LawyerSpecializationType(String description, int experienceLevel) {
+        this.description = description;
+        this.experienceLevel = experienceLevel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getExperienceLevel() {
+        return experienceLevel;
+    }
 }
